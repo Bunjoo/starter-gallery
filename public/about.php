7878cfbf2668c481e4c025adccc932e51f573894
arthur pankiewicz <arthurpankiewicz@gmail.com>
2016-01-15 21:55:26 -0800

    part 1 complete

--- public/about.php
+++ public/about.php
@@ -14,9 +14,9 @@
                 <span class="myhead">Simple Image Gallery</span>
                 <span class="mynav">
                     <ul>
-                        <li><a href="/welcome">Home</a></li>
-                        <li><a href="/gallery">Gallery</a></li>
-                        <li><a href="/">About</a></li>
+                        <li><a href="welcome.php">Home</a></li>
+                        <li><a href="gallery.php">Gallery</a></li>
+                        <li><a href="about.php">About</a></li>
                     </ul>
                 </span>
             </div>
