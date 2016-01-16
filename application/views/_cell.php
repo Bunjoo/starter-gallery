218a4e5831b69fb08d9fa0359781b19284b0a351
arthur pankiewicz <arthurpankiewicz@gmail.com>
2016-01-15 23:05:53 -0800

    part 3 completed

---
+++ application/views/_cell.php
@@ -0,0 +1,6 @@
+<?php
+?>
+<a href="./data/{filename}" data-lightbox="gallery"
+   data-title="{title} ... Posted {uploaded} by {uploader}, in {catgory}">
+    <img src="./data/thumb/{filename}"/>
+</a>
